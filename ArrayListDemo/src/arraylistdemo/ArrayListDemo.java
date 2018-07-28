c69d473e61eb422f88ed73b438c9e6644032094b
Lomovskoy <lomovskoy.kirill@yandex.ru>
2018-07-28 11:31:39 +0300

    Первая попытка

--- ArrayListDemo/src/arraylistdemo/ArrayListDemo.java
+++ ArrayListDemo/src/arraylistdemo/ArrayListDemo.java
@@ -1,5 +1,7 @@
 package arraylistdemo;
 
+import java.util.ArrayList;
+
 /**
  * Демонстрация использования ArrayList
  * @author lomov
@@ -11,6 +13,18 @@ public class ArrayListDemo {
      */
     public static void main(String[] args) {
         // Создать массив список
+        ArrayList<String> al = new ArrayList<String>();
+        System.out.println("Начальный размер коллекции: " + al.size());
+        
+        //Добавляем элеменны в коллекцию.
+        al.add("C");
+        al.add("A");
+        al.add("E");
+        al.add("B");
+        al.add("D");
+        al.add("F");
+        al.add(1, "C");
+        System.out.println("Начальный размер коллекции: " + al.size());
     }
     
 }
