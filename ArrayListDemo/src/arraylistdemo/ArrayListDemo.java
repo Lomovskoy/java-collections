b4af0903c7ac4e26a8faab78c90735d4ee8e9c49
Lomovskoy <lomovskoy.kirill@yandex.ru>
2018-07-28 11:10:46 +0300

    пробная функция

---
+++ ArrayListDemo/src/arraylistdemo/ArrayListDemo.java
@@ -0,0 +1,16 @@
+package arraylistdemo;
+
+/**
+ * Демонстрация использования ArrayList
+ * @author lomov
+ */
+public class ArrayListDemo {
+
+    /**
+     * @param args с командной строки
+     */
+    public static void main(String[] args) {
+        // Создать массив список
+    }
+    
+}
